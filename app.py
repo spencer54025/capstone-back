0b5abacc9a10f879fb70bae4a61198a0b17537c2
spencer54025 <spencervp54025@gmail.com>
2022-05-26 13:56:07 -0600

    changed directory

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
-import os
+# import os
 
 app = Flask(__name__)
 CORS(app)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+# basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://jjirsyzkowwrcx:a89361f80f7352158f85fd8841fa8f5dc35c7594bb93a49102efdbcf9d1e3d0d@ec2-3-228-235-79.compute-1.amazonaws.com:5432/daiabcc4gkelqr'
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
